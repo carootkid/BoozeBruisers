89a2800982d1d7b24db23f99e402bb65e630b64c
Destroyer10153 <144148275+AidenMoss@users.noreply.github.com>
2024-12-30 13:48:36 -0700

    Added basic turn system, made extra part ins ceen for testing, simplified code and added comments becayse yupurus stupid and oyu cnat read

--- Assets/_Scripts/ItemStuff/Item.cs
+++ Assets/_Scripts/ItemStuff/Item.cs
@@ -13,23 +13,32 @@ public class Item : MonoBehaviour
     public TMP_Text houseFireText;
     public TMP_Text TarotCardText;
     public ZonkManager zonkManager;
+    public TurnManager turnManager;
 
     void Start()
     {
+        //Finds zonk manager and sets zonk level to 0 (NEED TO CHANGE WITH TURN SYSTEM : need to make zonk level on a player by player basis)
         zonkManager = FindObjectOfType<ZonkManager>();
         zonkManager.ZonkLevel = 0f;
+        //Finds turn manager and sets it to player ones turn first (NEED TO CHANGE WITH TURN SYSTEM : coin flip for who starts)
+        turnManager = FindObjectOfType<TurnManager>();
+        turnManager.PlayerOneTurn = true;
 
+        //Turns off the text for the basic UI stuff as long as it exists
         if (leafLubberText != null) leafLubberText.gameObject.SetActive(false);
         if (triggerSappyText != null) triggerSappyText.gameObject.SetActive(false);
         if (mightyMoonshineText != null) mightyMoonshineText.gameObject.SetActive(false);
         if (houseFireText != null) houseFireText.gameObject.SetActive(false);
+        if (TarotCardText!= null) TarotCardText.gameObject.SetActive(false);
     }
     private void Update(){
+        //Makes zonk meter gooder
         ZonkMeterText.text = "Zonk Meter :" + zonkManager.ZonkLevel;
     }
 
     public void Click()
     {
+        //on click does stuff
         if (itemName == "Leaf Lubber")
         {
             Debug.Log("Clicked Leaf Lubber");
@@ -49,13 +58,21 @@ public class Item : MonoBehaviour
         {
             Debug.Log("Clicked House Fire");
             zonkManager.ZonkLevel += 50f;
-        }else if(itemName == "Tarot Cards"){
+        }
+        else if(itemName == "Tarot Cards")
+        {
             Debug.Log("Clicked Tarot Cards");
+        }
+        else if (itemName == "Player Two")
+        {
+            Debug.Log("Punching Player Two");
+            turnManager.PlayerOneTurn = false;
         }
     }
 
     public void ShowDescription(bool show)
     {
+        //shows the description of the stuff when you hover over it and makes it so the text equals whatever is the item description
         if (itemName == "Leaf Lubber" && leafLubberText != null)
         {
             leafLubberText.gameObject.SetActive(show);
---
+++ Assets/_Scripts/TurnManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool PlayerOneTurn;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
