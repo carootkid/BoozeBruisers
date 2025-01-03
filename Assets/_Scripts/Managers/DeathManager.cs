c93fbe07aef2228cf9b1153d6afd0d85aa544637
Destroyer10153 <144148275+AidenMoss@users.noreply.github.com>
2025-01-02 18:42:20 -0700

    death works

--- Assets/_Scripts/Managers/DeathManager.cs
+++ Assets/_Scripts/Managers/DeathManager.cs
@@ -6,6 +6,8 @@ public class DeathManager : MonoBehaviour
 {
     public ZonkManager zonkManager;
     public TurnManager turnManager;
+    public PlayerManager playerManager;
+    public float deathChanceMultiplier = 0.01f;
 
     void Start()
     {
@@ -13,8 +15,33 @@ public class DeathManager : MonoBehaviour
         turnManager = FindObjectOfType<TurnManager>();
     }
 
-    void Update()
+    public void deathChance()
     {
+        float playerOneDeathChance = zonkManager.PlayerOneZonkLevel * deathChanceMultiplier;
+        float playerTwoDeathChance = zonkManager.PlayerTwoZonkLevel * deathChanceMultiplier;
+
+        float randomValue = Random.value;
+
+        if (randomValue < playerOneDeathChance)
+        {
+            Debug.Log("Player 1 died");
+            EndGame();
+            return;
+        }
+
+        if (randomValue < playerTwoDeathChance)
+        {
+            Debug.Log("Player 2 died");
+            EndGame();
+            return;
+        }
+
+        Debug.Log("Survived");
+    }
+
+    private void EndGame()
+    {
+        Debug.Log("GAME OVER");
         
     }
 }
--- Assets/_Scripts/Managers/PlayerManager.cs
+++ Assets/_Scripts/Managers/PlayerManager.cs
@@ -6,6 +6,7 @@ public class PlayerManager : MonoBehaviour
 {
     [Header("Manager")]
     public TurnManager turnmanager;
+    public DeathManager deathManager;
 
     [Header("Sources")]
     public AudioSource leftSoft;
@@ -50,6 +51,7 @@ public class PlayerManager : MonoBehaviour
     }
 
     void PunchEnded(){
+        deathManager.deathChance();
         turnmanager.PlayerOneTurn = !turnmanager.PlayerOneTurn;
         turnmanager.punching = false;
     }
