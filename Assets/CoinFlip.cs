fc4e44a9d9d5eceb3c92c1bbcc648dfd1061db56
CarootMan <chase.banbrough@gmail.com>
2025-01-07 15:39:14 -0700

    well, you can flip coins now

---
+++ Assets/CoinFlip.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+public class CoinFlip : MonoBehaviour
+{
+    [Header("Variables")]
+    public Animator coinAnim;
+    public bool playerOneOnHeads;
+    private int coinHead;
+    public bool playerOneWon = false;
+    [Header("Objects")]
+    public GameObject headBut;
+    public GameObject tailBut;
+    public GameObject playerOneWins;
+    public GameObject playerTwoWins;
+    public GameObject infoText;
+    [Header("Audio")]
+    public AudioSource src;
+    public AudioClip clp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        coinHead = UnityEngine.Random.Range(0, 2);
+        Debug.Log(coinHead);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void coinFlipAudio(){
+        src.PlayOneShot(clp);
+    }
+
+    void Heads(){
+        playerOneOnHeads = true;
+        flip();
+    }
+
+    void Tails(){
+        playerOneOnHeads = false;
+        flip();
+    }
+    void flip(){
+        tailBut.SetActive(false);
+        headBut.SetActive(false);
+        infoText.SetActive(false);
+        if(coinHead == 0){
+            coinAnim.SetTrigger("Heads");
+        } else {
+            coinAnim.SetTrigger("Tails");
+        }
+    }
+
+    void revealWinner(){
+        if(playerOneOnHeads == true && coinHead == 0){
+            playerOneWins.SetActive(true);
+            playerOneWon = true;
+        } else if(playerOneOnHeads == false && coinHead == 1){
+            playerOneWins.SetActive(true);
+            playerOneWon = true;
+        } else {
+            playerTwoWins.SetActive(true);
+            playerOneWon = false;
+        }
+    }
+}
